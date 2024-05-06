0c5d5d61f1e49f7f10080a19c8ca75bc2b715b5b
Eguis Suarez <58845596+Eguis16@users.noreply.github.com>
2024-05-06 15:22:12 -0400

    Add files via upload

--- Sysprep/main.ps1
+++ Sysprep/main.ps1
@@ -1,4 +1,11 @@
-# Codigo creado por Eguis Suarez especialista TI Fcom Spa
+#Codigo creado por Eguis Suarez especialista TI Fcom Spa
+#Este codigo esta basado en el Script creado por Jorge Santos Jefe de Soporte TI Fcom Spa
+<#Funciones del software: 
+- Eliminacion de aplicativos preinstalados de Windows
+- Asignacion de nombre de imagen y sistema
+- Ejecucion de Sysprep de forma desantendida#>
+
+
 #Autoelevacion de Script main.ps1
 function AdminPrivileges {
     # Verificar si el usuario tiene privilegios de administrador
@@ -30,45 +37,60 @@ function Bloatware () {
     }
 }
 
-#Generacion de Nombre de Imagen
-function NombreMaquina {
-    #declaracion de variables
-    $Manufacturer = (Get-WmiObject -Class Win32_ComputerSystem).Manufacturer
-    $ModelHP = (Get-WmiObject -Class Win32_ComputerSystem).Model
-    $Sku = (Get-WmiObject -Class Win32_ComputerSystem).SystemSKUNumber
-    $LenovoModel = (Get-WmiObject -Class Win32_ComputerSystemProduct).Version
-    $MtM = (Get-WmiObject -Class Win32_ComputerSystem).Model
-
-    #Obtecion de nombre del Sistema Operativo
-    $osname = (Get-WmiObject -Class Win32_OperatingSystem).Name
-    $osname = $osname -split '\|' | Select-Object -First 1
-    if($osname -eq "Microsoft Windows 10 Pro") { $osname = "W10PRO"}
-    elseif ($osname -eq "Microsoft Windows 11 Pro") { $osname = "W11PRO"}
-    elseif ($osname -eq "Microsoft Windows 11 Pro Education") {$osname = "W11PROEDU"}
-    #Obtencion de version del SO
-    $Version = (Get-ItemProperty -Path "HKLM:\\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name "DisplayVersion").DisplayVersion
-
-    #Solicitud de informacion al usuario
-    Clear-Host
-    Write-Host "Indique la porcion del nombre de la imagen que corresponde al Cliente"
-    Write-Host "Ejemplo: >CLIENTE<_W11PRO23H2_MODELO_PN"
-    $Client = Read-Host "Cliente"
-    Clear-Host
-    Write-Host "Indique la porcion del nombre de la imagen que corresponde al Modelo"
-    Write-Host "Ejemplo: >CLIENTE<_W11PRO23H2_MODELO_PN"
-    Write-Host "Ejemplo Modelo HP: EliteBook 640 G10 >640G10"
-    Write-Host "Ejemplo Modelo Lenovo: Lenovo T14 Gen 3 > T14GEN3"
-    $modelo = Read-Host "Modelo"
+#Creacion de cuenta de local administrador
+function CuentadeUsuario {
+    #Declaracion de Variables
+    $Cuenta = Read-Host "Cuenta"
+    $Password = Read-Host -AsSecureString "Contrasena"
     
-    #Actualizacion de registro
-    if ($Manufacturer -eq "HP" -or $Manufacturer -eq "Hewllet-Packard") {
-        $model = "$modelhp | $client" + "_$osname$version_$modelo_$sku"
-        New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" -Name "Model" -Value $model -PropertyType "String" -Force | Out-Null
+    # Parameter help description
+    try {
+        #Creacion de cuenta
+        New-LocalUser -Name $Cuenta -Password $Password -AccountNeverExpires -PasswordNeverExpires
+        Write-Host "La cuenta $Cuenta se ha creado exitosamente"
+        Pause 
+        
+        #Agregar al usuario a miembro administradores
+        Add-LocalGroupMember -Group "Administradores" -Member $Cuenta
+        Write-Host "La cuenta '$Cuenta' se ha agregado exitosamente al grupo administradores"
+        Pause
     }
+    catch {
+        Write-Host "Se ha presentado un problema con la cuenta '$Cuenta'"
+    }
+}
+
+#Generacion de Nombre de Imagen
+function ComputerModel {
+    #Declaracion de variables
+    $Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+    $ModelHP = (Get-CimInstance -ClassName Win32_ComputerSystem).Model
+    $Sku = (Get-CimInstance -ClassName Win32_ComputerSystem).SystemSKUNumber
+    $ModelLenovo = (Get-CimInstance -ClassName Win32_ComputerSystemProduct).Version
+    $Mtm = (Get-CimInstance -ClassName Win32_ComputerSystem).Model
+    $OSVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name "DisplayVersion").DisplayVersion
+    $OSName = (Get-CimInstance -ClassName Win32_OperatingSystem).Name -split '\|' | Select-Object -First 1
+    if ($OSName -eq "Microsoft Windows 10 Pro") {$OSName= "W10PRO"}
+    elseif ($OSName -eq "Microsoft Windows 11 Pro") {$OSName = "W11PRO"}
+    elseif ($OSName -eq "Microsoft Windows 11 Pro Education") {$OSName = "W11PROEDU"}
+    #Llamado
+    $Cliente = Read-Host "Cliente"
+    $Modelo = Read-Host "Modelo"
+
+    #Asignacion
+    if ($Manufacturer -eq "HP" -or $Manufacturer -eq "Hewlett-Packard") {
+        $Model = "$ModelHP | $Cliente" + "_$OSName$OSVersion" + "_$Modelo" + "_$Sku"
+        $SystemName = (Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" -Name "Model" -Value $Model -Type String -Force | Out-Null)
+        
+    } 
     elseif ($Manufacturer -eq "LENOVO") {
-        $model = "$modellenovo | $cliente" + "_$osname$version_$modelo_sku"
-        New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" -Name "Model" -Value $model -PropertyType "String" -Force | Out-Null       
+        $Model = "$ModelLenovo | $Cliente" + "_$OSName$OSVersion" + "_$Modelo" + "$Mtm"
+        $SystemName = (Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" -Name "Model" -Value $Model -Type String -Force | Out-Null)
+    } else {
+        Write-Host "Se ha producido un error en el proceso de asignacion de nombre de imagen"
     }
+    Write-Host = "Nombre Asignado -->" + $SystemName
+    Pause
 }
 
 
@@ -82,20 +104,25 @@ function Invoke_Sysprep {
     Write-Host
 }
 
+
 function Show-Menu {
     Clear-Host
     Write-Host "1. Eliminacion de aplicativos Bloatware de Microsoft"
-    Write-Host "2. Preparacion de imagen (Sysprep)"
-    Write-Host "3. Exit"
-    $seleccion = Read-Host "Elija una opcion"
+    Write-Host "2. Asignacion de nombre de Imagen"
+    Write-Host "3. Creacion de cuenta local del Cliente"
+    Write-Host "4. Preparacion de imagen (Sysprep)"
+    Write-Host "5. Exit"
+    $seleccion = Read-Host "Elija una opcion: "
 
     switch ($seleccion) {
         '1' {Bloatware; Show-Menu}
-        '2' {Invoke_Sysprep; Show-Menu}
-        '3' {Exit}
+        '2' {ComputerModel; Show-Menu}
+        '3' {CuentadeUsuario; Show-Menu}
+        '4' {Invoke_Sysprep; Show-Menu}
+        '5' {Exit}
         default {Write-Host "Opcion invalida. Porfavor intente de nuevo."; Show-Menu}
     }
-    
+
 }
 
 Show-Menu
